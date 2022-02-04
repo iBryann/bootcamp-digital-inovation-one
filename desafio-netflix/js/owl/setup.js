8ba9eb82f636a4106e75177fe8649aa394f5dc09
BryannWine <pj.bryann@wine.com.br>
2022-02-04 11:47:12 -0300

    Desafio Netflix concluído.

---
+++ desafio-netflix/js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    nav: false,
+    loop: false,
+    margin: 10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
